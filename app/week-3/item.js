aaa40fa02b73aaf13f73b8fc13e179d784bc440a
rudra-verma-10 <rudra.verma@edu.sait.ca>
2024-02-06 15:31:51 -0700

    added tailwind css

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 const Item = ({ name, quantity, category }) => {
   return (
-   <ul>
-    <li>
+   <ul className="bg-blue-300 p-4 w-1/3 mt-6 mx-4">
+    <li className="font-bold text-xl">
       Name: {name}
     </li>
     <li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from './item-list';
 
 export default function Page() {
   return (
-    <main>
-      <h1>Shopping list</h1>
+    <main >
+      <h1 className='font-bold text-4xl p-2'>Shopping list</h1>
       <ItemList />
     </main>
   );
